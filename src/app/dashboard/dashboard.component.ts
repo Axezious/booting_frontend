c6e4af2e62e6d8f40a9b2733cd6663fbaa703574
Avogadro <gibranadhifattahputra@gmail.com>
2020-08-13 15:01:19 +0700

    update tampilan dashboard by Avogadro

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -15,25 +15,25 @@ export class DashboardComponent implements OnInit {
   date: Date = new Date();
 
   visitSaleChartData = [{
-    label: 'CHN',
+    label: 'Open',
     data: [20, 40, 15, 35, 25, 50, 30, 20],
     borderWidth: 1,
     fill: false,
   },
   {
-    label: 'USA',
+    label: 'Close',
     data: [40, 30, 20, 10, 50, 15, 35, 40],
     borderWidth: 1,
     fill: false,
   },
   {
-    label: 'UK',
+    label: 'Reopen',
     data: [70, 10, 30, 40, 25, 50, 15, 30],
     borderWidth: 1,
     fill: false,
   }];
 
-  visitSaleChartLabels = ["2013", "2014", "2014", "2015", "2016", "2017"];
+  visitSaleChartLabels = ["Jan", "Feb", "Mar", "Apr", "Mei", "Jun"];
 
   visitSaleChartOptions = {
     responsive: true,
