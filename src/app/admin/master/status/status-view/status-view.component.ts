63b3e72be19696996ac213fed7a709cfac16fcc4
anastasiusmanurung <anastasiusmanurung@gmail.com>
2020-09-02 12:13:52 +0700

    perbaikanuserdanstatus

--- src/app/admin/master/status/status-view/status-view.component.ts
+++ src/app/admin/master/status/status-view/status-view.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { StatusService } from 'src/app/service/master/status.service';
+import { ApiService } from 'src/app/service/api.service';
+import { Status } from 'src/app/model/status';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-status-view',
   templateUrl: './status-view.component.html',
-  styleUrls: ['./status-view.component.scss']
+  styleUrls: ['./status-view.component.scss'],
+  providers: [
+    StatusService
+  ]
 })
 export class StatusViewComponent implements OnInit {
 
-  constructor() { }
+  status$: Observable<Status[]>;
+  total$: Observable<number>;
+
+  constructor(private service: StatusService, private apiService: ApiService) {
+    this.status$ = service.status$;
+    this.total$ = service.total$;
+  }
 
   ngOnInit() {
+  }
+
+  async deleteStatus(status: Status) {
+    this.apiService.deleteStatus(status).subscribe(status => {
+      console.log(status);
+      this.service.viewStatus();
+    })
   }
 
 }
