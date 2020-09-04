2e1c2757cf2f6a507f0720cce42c2e802728e3b6
Avogadro <gibranadhifattahputra@gmail.com>
2020-09-04 11:33:41 +0700

    test

--- src/app/client/list-ticket/list-ticket.component.ts
+++ src/app/client/list-ticket/list-ticket.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, QueryList, ViewChildren, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { ApiService } from '../../service/api.service';
+
+import { Tickets } from '../../model/tickets';
+import { ListTicketsService } from '../../service/list-ticket.service';
 
 @Component({
   selector: 'app-list-ticket',
   templateUrl: './list-ticket.component.html',
-  styleUrls: ['./list-ticket.component.scss']
+  styleUrls: ['./list-ticket.component.scss'],
+  providers: [
+    ListTicketsService
+  ]
 })
 export class ListTicketComponent implements OnInit {
 
-  constructor() { }
+  tickets$:Observable<Tickets[]>;
+  ticketsOpen$:Observable<Tickets[]>;
+  total$:Observable<number>;
+
+  constructor(private apiService:ApiService, private service:ListTicketsService) { 
+  	this.tickets$ = service.tickets$;
+    this.total$ = service.total$;
+  }
 
   ngOnInit() {
+  }
+
+  async getListTicket() {
+  	this.apiService.getListTicket().subscribe(result => {
+  		console.log(result);
+  	})
+  }
+
+  getOpenTickets() {
+    this.service.openFilter();
+  }
+
+  getCloseTickets() {
+
+  }
+
+  getReopenTickets() {
+
   }
 
 }
